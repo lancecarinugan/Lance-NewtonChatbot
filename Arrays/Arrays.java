8434222658f7e370353fe90d668aaa0498ccfcda
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-09-21 11:40:20 -0700

    Started not done

---
+++ Arrays/Arrays.java
@@ -0,0 +1,9 @@
+
+public class Arrays
+{
+    public static void main(String[] args) 
+    {
+        String[] array ={"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int i = 0; i < myArray.length; i++)
+    }
+}
